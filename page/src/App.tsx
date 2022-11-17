3adde3fc16dc996a63aeb62dce1e5aae0862395c
Aurora Zuoris <programming.aurora@gmail.com>
2022-11-17 11:59:36 +0000

    Finished report & radar tabs.

--- page/src/App.tsx
+++ page/src/App.tsx
@@ -1,43 +1,119 @@
-import { Box, Container, Paper, Stack, Tab, TabTypeMap, Typography } from "@mui/material";
+import { Box, Container, Paper, Stack, Tab, Typography } from "@mui/material";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
 import React, { useState } from "react";
 import { Legend, PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart } from "recharts";
 
 type TabName = "report" | "radar" | "SUS";
 
+type ReportSection = { header: string; body: string };
+
+const reportSections: ReportSection[] = [
+	{
+		header: "Strive for consistency",
+		body: "Both sites are well designed in this regard. "
+			+ "Youtube Music follows Material Design, the same design guideline that is used by all of Google's products. "
+			+ "And Spotify also has its own consistent style across its entire web player."
+	},
+	{
+		header: "Seek universality",
+		body: "When it comes to universality, Spotify takes the lead. "
+			+ "On spotify's web app, you can tab through all the elements "
+			+ "in the same order as they are rendered. "
+			+ "Meanwhile Youtube Music can't be reliably tabbed trough, "
+			+ "sometimes even skipping some elements that you would expect to be focused on next."
+	},
+	{
+		header: "Offer informative feedback",
+		body: "Both sites are half decent in this regard. "
+			+ "They both show extra information when the user hovers over an element. "
+			+ "And they both also use snackbar messages about the actions that the user has taken, however, they aren't shown on all actions. "
+			+ "For example, Spotify only shows a snackbar upon the adition of a song to a playlist, but not its removal. "
+			+ "Meanwhile YouTube does the opposite, showing a snackbar when a song is removed from a playlist, but not when it is added."
+	},
+	{
+		header: "Design dialogs to yield closure",
+		body: "The only dialog that both sites have is used for payment. "
+			+ "And both sites do a good job at it. "
+			+ "They both start with a clear message about what the user is about to pay for, and they both give the user options on how to pay for it. "
+			+ "Showing more elements as the user progresses trough the dialog. "
+			+ "Finally, both sites also show a summary of the payment before the user confirms it."
+			+ "Finishing with a clear message about what the user has just paid for."
+	},
+	{
+		header: "Prevent errors",
+		body: "Both sites are fairly good at preventing errors, as they both use confirmation modals for destructive actions. "
+			+ "and in the case of Youtube Music, when it doesn't show a confirmation modal, it shows a snackbar with an undo button."
+
+	},
+	{
+		header: "Permit easy reversal of actions",
+		body: "When it comes to action reversal, Youtube Music is much better than Spotify, "
+			+ "as it allows the user to undo the removal of a song from a playlist. "
+			+ "While Spotify doesn't have that at all. "
+			+ "But even then, neither let you undo the deletion of a playlist, so both sides also have some room for improvement."
+
+	},
+	{
+		header: "Keep users in control",
+		body: "While both sites don't have a propper settings page, "
+			+ "they do have a player widget at the bottom of the screen that can be used to control the player. "
+	},
+	{
+		header: "Reduce short-term memory load",
+		body: "Both sites are fairly good in this regard. "
+			+ "Upon opening either site, the last song you were listening to is displayed on the playback controlelr at the bottom. "
+			+ "This is a good way to remind the user of what they were listening to, and also allows them to quickly resume playback. "
+			+ "And in adition, the landing sites of both of these web aps show you the most recently played songs, artists, and albums."
+	},
+];
+
 const Report = () => {
 	return (
-		<Box sx={{ height: "100%", width: "100%" }}>
-			<Typography variant="h1">Report</Typography>
-		</Box>
+		<Paper sx={{ height: "100%", width: "100%", p: 4 }}>
+			<Typography paragraph>
+				In this analysis, we will study two big websites used for streaming music:
+			</Typography>
+			<ul>
+				<li><Typography variant="body1" component="a" href="https://music.youtube.com">Youtube</Typography></li>
+				<li><Typography variant="body1" component="a" href="https://open.spotify.com">Spotify</Typography></li>
+			</ul>
+			{reportSections.map(({ header, body }) => (
+				<>
+					<Typography variant="h4" sx={{ mt: 3 }}>{header}</Typography>
+					<Typography>{body}</Typography>
+				</>
+			))}
+		</Paper>
 	);
 };
 
-const radarData: any[] = [
-	{ rule: "Consistency", yt: 10, sp: 10, fullMark: 10},
-	{ rule: "Universal", yt: 1, sp: 7, fullMark: 10},
-	{ rule: "Informative", yt: 10, sp: 10, fullMark: 10},
-	{ rule: "Closure", yt: 5, sp: 10, fullMark: 10},
-	{ rule: "Error Prevention", yt: 10, sp: 10, fullMark: 10},
-	{ rule: "Action reversal", yt: 8, sp: 3, fullMark: 10},
-	{ rule: "Control", yt: 8, sp: 8, fullMark: 10},
-	{ rule: "Memory", yt: 10, sp: 10, fullMark: 10},
+type RadarData = { rule: string; yt: number; sp: number, fullMark: number };
+
+const radarData: RadarData[] = [
+	{ rule: "Consistency", yt: 10, sp: 9, fullMark: 10 },
+	{ rule: "Universal", yt: 1, sp: 6, fullMark: 10 },
+	{ rule: "Informative", yt: 6, sp: 5, fullMark: 10 },
+	{ rule: "Closure", yt: 8, sp: 7, fullMark: 10 },
+	{ rule: "Error Prevention", yt: 9, sp: 8, fullMark: 10 },
+	{ rule: "Action reversal", yt: 8, sp: 3, fullMark: 10 },
+	{ rule: "Control", yt: 8, sp: 8, fullMark: 10 },
+	{ rule: "Memory", yt: 9, sp: 9, fullMark: 11 },
 ];
 
 const RadarTab = () => {
 	return (
 		<Paper sx={{ height: "100%", width: "100%", p: 10 }}>
 			<RadarChart
-				width={500}
-				height={500}
+				width={600}
+				height={380}
 				data={radarData}
 			>
-				<PolarGrid/>
-				<PolarAngleAxis dataKey="rule"/>
-				<PolarRadiusAxis angle={45} domain={[0, 10]}/>
-				<Radar name="Youtube Music" dataKey="yt" stroke="#FF0000" fill="#FF0000" fillOpacity={0.3}/>
-				<Radar name="Spotify" dataKey="sp" stroke="#1DB954" fill="#1DB954" fillOpacity={0.3}/>
-				<Legend/>
+				<PolarGrid />
+				<PolarAngleAxis dataKey="rule" />
+				<PolarRadiusAxis angle={45} domain={[0, 10]} />
+				<Radar name="Youtube Music" dataKey="yt" stroke="#FF0000" fill="#FF0000" fillOpacity={0.3} />
+				<Radar name="Spotify" dataKey="sp" stroke="#1DB954" fill="#1DB954" fillOpacity={0.3} />
+				<Legend />
 			</RadarChart>
 		</Paper>
 	);
@@ -48,7 +124,7 @@ const tableRows = [
 
 const SUSTab = () => {
 	return (
-		<Paper sx={{p: 2}}>
+		<Paper sx={{ p: 2 }}>
 			A
 		</Paper>
 	);
@@ -59,12 +135,12 @@ const App = () => {
 	const setTab = (e: React.SyntheticEvent, newValue: string) => setOpenTab(newValue as TabName);
 
 	return (
-		<Container sx={{mt: 20}}>
-			<Stack sx={{mb: 10}} direction="row" alignItems="center" justifyContent="center" gap={5}>
+		<Container sx={{ mt: 20 }}>
+			<Stack sx={{ mb: 10 }} direction="row" alignItems="center" justifyContent="center" gap={5}>
 				<Typography
 					variant="h2"
 					component="a"
-					sx={{textDecoration: "none", color: "inherit"}}
+					sx={{ textDecoration: "none", color: "inherit" }}
 					href="https://music.youtube.com"
 				>
 					Youtube Music
@@ -73,23 +149,23 @@ const App = () => {
 				<Typography
 					variant="h2"
 					component="a"
-					sx={{textDecoration: "none", color: "inherit"}}
+					sx={{ textDecoration: "none", color: "inherit" }}
 					href="https://open.spotify.com"
 				>
 					Spotify
 				</Typography>
 			</Stack>
 			<TabContext value={openTab}>
-				<Box sx={{borderBottom: 1, borderColor: "divider"}}>
+				<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
 					<TabList onChange={setTab}>
-						<Tab label="Report" value="report"/>
-						<Tab label="Radar" value="radar"/>
-						<Tab label="SUS" value="SUS"/>
+						<Tab label="Report" value="report" />
+						<Tab label="Radar" value="radar" />
+						<Tab label="SUS" value="SUS" />
 					</TabList>
 				</Box>
-				<TabPanel value="report"><Report/></TabPanel>
-				<TabPanel value="radar"><RadarTab/></TabPanel>
-				<TabPanel value="SUS"><SUSTab/></TabPanel>
+				<TabPanel value="report"><Report /></TabPanel>
+				<TabPanel value="radar"><RadarTab /></TabPanel>
+				<TabPanel value="SUS"><SUSTab /></TabPanel>
 			</TabContext>
 		</Container>
 	);
--- report.md
+++ report.md
@@ -17,34 +17,37 @@ When it comes to universality, Spotify takes the lead.
 On Spotify's web app you can tab through all the elements
 in the same order as they are rendered.
 Meanwhile YouTube Music can't be reliably tabbed trough,
-sometimes even skipping some selectable elements
+sometimes even skipping some elements
 that you would expect to be focused on next.
 
 # Offer informative feedback
 
 Both sites are half decent in this regard.
-They mostly achieve this trough the use of toast messages,
-but they aren't done on all actions.
-For example, Spotify shows a toast message upon the adition of a song to a playlist,
-but not on the removal of a song from a playlist.
-Meanwhile Youtube Music does the opposite,
-only showing a toast message upon the removal of a song from a playlist,
-but not on adition.
+They both show extra information when the user hovers over an element.
+And they both also use snackbar messages about
+the actions that the user has taken,
+however, they aren't shown on all actions.
+For example, Spotify only shows a snackbar
+upon the adition of a song to a playlist, but not its removal.
+Meanwhile YouTube does the opposite,
+showing a snackbar when a song is removed from a playlist,
+but not when it is added.
 
 # Design dialogs to yield closure
 
-The only dialog that both sites have is used for payment.
-And both sites do a good job at this.
+The only dialog that both sites have is used for payment,
+and both sites do a good job at it.
 They both start with a clear message of what the user is about to pay for,
 and then they both give the user options on how to pay for it.
 Showing more elements as the user progresses trough the dialog.
+Finally, both sites also show a summary of the payment before the user confirms it.
 Finishing with a clear message of what the user has just paid for.
 
 # Prevent errors
 
 Both sites are fairly good in this regard, as they both use confirmation
 modals for destructive actions,
-and in the case of Youtube Music, when it doesn't show a modal, it lets the user to undo the action.
+and in the case of Youtube Music, when it doesn't show a modal, shows a snackbar with an undo button.
 
 # Permit easy reversal of actions
 
@@ -56,13 +59,13 @@ have some room for improvement.
 
 # Keep users in control
 
-While both sites don't have a settings page,
-both sites have a player control bar at the bottom of the screen,
-which fully controls the music being playbacked.
+While both sites don't have a propper settings page,
+they do have a player widget at the bottom of the screen
+that can be used to control the playback.
 
 # Reduce short-term memory load
 
 Both sites are fairly good in this regard.
-Upon opening either site, the last song played is displayed in the playback controller at the bottom,
-letting the user to continue listening where they stopped in the last session.
-And in adition, the landing sites of both of these web apps 
+Upon opening either site, the last song you were listening to is displayed in the playback controller at the bottom.
+This is a good way to remind the user of what they were listening to, and also allows them to quickly resume playback.
+And in adition, the landing sites of both of these web apps show you the most recently played songs, artists and albums.
